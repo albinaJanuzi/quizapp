826549fdf368a35141566f306277307b0e156410
Albina Januzi <albina_januzi@hotmail.com>
2024-12-11 13:44:21 +0100

    JSON array

---
+++ script.js
@@ -0,0 +1,19 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Eilliams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+
+    {
+        "question": "Wann wurde HTML entwickelt?",
+        "answer_1": "1992",
+        "answer_2": "1995",
+        "answer_3": "1993",
+        "answer_4": "2001",
+        "right_answer": 1
+    }
+];
